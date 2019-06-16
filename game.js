9550dd5c5458b31b57e28ee21ae8371a51b66507
Paulo Liedtke <liedtke.paulo@gmail.com>
2019-06-16 16:53:14 +0200

    noch paar js funktionlitäten hinzugefügt damit das spiel mehr spaß macht

--- game.js
+++ game.js
@@ -32,54 +32,81 @@ let questions = [
 "Mach einen Bauchtanz für 1 Minute"
 ]
 
+let alreadyClicked = [false, false, false, false, false, false, false, false, false, false,
+  false, false, false, false, false, false, false, false, false, false];
+
+let checkForWinTeam1 = 0;
+let checkForWinTeam2 = 0;
+
 function confirmDare(){
   if(window.confirm("Willst du eine Aufgabe machen?")) return true
   else return false;
 }
 
 //unterste Funktion
-function cupClicked(id){
-  let choice = confirmDare();
-  if(choice == true){
-    let random = Math.floor(Math.random() * questions.length) //zufällige Zahl 0 bis questions.length
-    document.getElementById("aufgabenfeld").innerHTML = questions[random]; //ändert den text des aufgabenfeldes
+function cupClicked(id, num){
+  if(alreadyClicked[num] == false){
+    let choice = confirmDare();
+    if(choice == true){
+      let random = Math.floor(Math.random() * questions.length) //zufällige Zahl 0 bis questions.length
+      document.getElementById("aufgabenfeld").innerHTML = questions[random]; //ändert den text des aufgabenfeldes
+      document.getElementById(id).style.border = "solid black 1px"
+      alreadyClicked[num] = true;
+    }
+    else{
+      //document.getElementById(id).style.display="none";
+      document.getElementById(id).style.opacity = 0.2; //setzt den becher transparent
+      console.log("Set " + id + " invisible");
+      document.getElementById("aufgabenfeld").innerHTML = ""; //zurücksetzen des aufgabenfeldes
+
+      if(num > 9) checkForWinTeam2++;
+      else checkForWinTeam1++;
+    }
   }
   else{
     //document.getElementById(id).style.display="none";
     document.getElementById(id).style.opacity = 0.2; //setzt den becher transparent
     console.log("Set " + id + " invisible");
     document.getElementById("aufgabenfeld").innerHTML = ""; //zurücksetzen des aufgabenfeldes
+
+    if(num > 9) checkForWinTeam2++;
+    else checkForWinTeam1++;
   }
+
+  console.log("Team Blau: " + checkForWinTeam1);
+  console.log("Team Rot: " + checkForWinTeam2);
+  if(checkForWinTeam1 == 10) window.alert("Team Blau gewinnt");
+  if(checkForWinTeam2 == 10) window.alert("Team Rot gewinnt");
 }
 
 
 //funktion zum event handeln
-function checkForEvents(id){
+function checkForEvents(id, num){
   document.getElementById(id).addEventListener("click", function() {
-      cupClicked(id);
+      cupClicked(id, num);
   }, false);
 }
 
 
 //aufruf der funktion für die verschiedenen becher
-checkForEvents("t10");
-checkForEvents("t11");
-checkForEvents("t12");
-checkForEvents("t13");
-checkForEvents("t14");
-checkForEvents("t15");
-checkForEvents("t16");
-checkForEvents("t17");
-checkForEvents("t18");
-checkForEvents("t19");
+checkForEvents("t10", 0);
+checkForEvents("t11", 1);
+checkForEvents("t12", 2);
+checkForEvents("t13", 3);
+checkForEvents("t14", 4);
+checkForEvents("t15", 5);
+checkForEvents("t16", 6);
+checkForEvents("t17", 7);
+checkForEvents("t18", 8);
+checkForEvents("t19", 9);
 
-checkForEvents("t20");
-checkForEvents("t21");
-checkForEvents("t22");
-checkForEvents("t23");
-checkForEvents("t24");
-checkForEvents("t25");
-checkForEvents("t26");
-checkForEvents("t27");
-checkForEvents("t28");
-checkForEvents("t29");
+checkForEvents("t20", 10);
+checkForEvents("t21", 11);
+checkForEvents("t22", 12);
+checkForEvents("t23", 13);
+checkForEvents("t24", 14);
+checkForEvents("t25", 15);
+checkForEvents("t26", 16);
+checkForEvents("t27", 17);
+checkForEvents("t28", 18);
+checkForEvents("t29", 19);
