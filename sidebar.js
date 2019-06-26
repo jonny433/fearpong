bd4cdf5b2c087ef22ae1943e56836cbe38c8d848
sam <schmidt.samuel@mailfence.com>
2019-06-26 18:11:11 +0200

    Schließ Button und Menü buton auf gleicher Stelle.
    Der eine wird außerdem unsichtbar

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,11 @@
 function togglemenu(){
 	document.getElementById('sidebar').style.top = "210px";
+    document.querySelector('.toggle-btn').style.display="none";
     document.body.style.overflow="hidden";
 }
 function closemenu(){
 	document.getElementById('sidebar').style.top = "-500%";
     document.body.style.overflow="visible";
+    document.querySelector('.toggle-btn').style.display="block";
     
 }
