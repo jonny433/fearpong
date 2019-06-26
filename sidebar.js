53c94c97c57315ce184705008486b4c56f6c9a34
sam <schmidt.samuel@mailfence.com>
2019-06-26 17:37:05 +0200

    sidebar reagiert nicht merh auf scrollen

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,9 @@
 function togglemenu(){
 	document.getElementById('sidebar').style.top = "210px";
+    document.body.style.overflow="hidden";
 }
 function closemenu(){
 	document.getElementById('sidebar').style.top = "-500%";
+    document.body.style.overflow="visible";
     
 }
